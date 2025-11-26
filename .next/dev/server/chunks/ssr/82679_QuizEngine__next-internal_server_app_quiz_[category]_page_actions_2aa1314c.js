df227a6f6ff8e504698171294c3efa4779d00f29
Omkar <omkarsolanke50@gmail.com>
2025-11-26 18:28:23 +0530

    profile change

---
+++ .next/dev/server/chunks/ssr/82679_QuizEngine__next-internal_server_app_quiz_[category]_page_actions_2aa1314c.js
@@ -0,0 +1,7 @@
+module.exports = [
+"[project]/OneDrive/Desktop/Quiz_engine/QuizEngine/.next-internal/server/app/quiz/[category]/page/actions.js [app-rsc] (server actions loader, ecmascript)", ((__turbopack_context__, module, exports) => {
+
+}),
+];
+
+//# sourceMappingURL=82679_QuizEngine__next-internal_server_app_quiz_%5Bcategory%5D_page_actions_2aa1314c.js.map
